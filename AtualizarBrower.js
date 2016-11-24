ca0f127dac83f67f9840da3521eeeed5ff585505
Cleyton Souza <cleytonnac@gmail.com>
2016-11-24 14:25:16 -0200

    AtualizaBrowserJS

---
+++ AtualizarBrower.js
@@ -0,0 +1,23 @@
+Atualizar automaticamente
+
+<!--
+ <script>
+ $(document).ready(function(){
+ setInterval(function(){cache_clear()},900000);
+ });
+ function cache_clear()
+{
+ window.location.reload(true);
+}
+</script>
+-->
+
+<!--
+1min = 60000
+2min = 120000
+5min = 300000
+8min = 480000
+10min = 600000
+15min = 900000
+20min = 1,2e+6
+-->
